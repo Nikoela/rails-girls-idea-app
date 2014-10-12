c3243d73c7240bd6f23adc991fa7bd9d926f53dd
Nikoela Aramova <nikoela@gmail.com>
2014-10-12 14:04:33 +0000

    Pass ideas from controller to view

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,10 @@
 class IdeasController < ApplicationController
   def index
+    @ideas = [ 
+  {id: 0, title:'My first idea'},
+  {id: 1, title:"Toster" },
+  {id: 2, title:"Toster" }
+      ]
   end
 end
 
