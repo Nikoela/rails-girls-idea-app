b264da177c7a9112a6e6b771a66047dc7c4c2619
Nikoela Aramova <nikoela@gmail.com>
2014-10-12 14:58:10 +0000

    Allow adding of ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,7 +2,16 @@ class IdeasController < ApplicationController
   def index
  
       @ideas = Idea.all
-      
+  end
+  
+  def new
+    @idea = Idea.new
+  end
+
+  def create
+    Idea.create(params.require(:idea).permit(:title, :description))
+    
+    redirect_to action: :index
   end
 end
 
